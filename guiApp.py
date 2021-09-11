5e8cac7ec4a885d0e25267c043a21c2035241764
Abem Lucas <abemlucas12@gmail.com>
2021-09-10 20:34:12 -0700

    Updated guiApp.py

--- guiApp.py
+++ guiApp.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 import os
+from tkinter.constants import CENTER, LEFT, RIGHT, TOP
 
 root = tk.Tk()
 apps = []
@@ -39,6 +40,14 @@ def runApps():
         os.startfile(app)
 
 
+# Function for removing or reseting the saved apps.
+
+def reset(reRun):
+
+    os.remove('opened_apps.txt')
+    reRun.forget()
+
+
 # set up the Canvas of the display screen.
 canvas = tk.Canvas(root, height=450, width=450, bg="black")
 canvas.pack()
@@ -49,19 +58,23 @@ frame.place(relwidth=1.0, relheight=1.0, relx=0.0, rely=0.0)
 
 # Initialize the variable for the button openFile.
 openFile = tk.Button(root, text="Open Files", padx=10,
-                     pady=5, fg="white", bg="red", command=addApp)
-openFile.pack()
+                     pady=5, fg="black", bg="white", command=addApp)
+openFile.pack(side=TOP)
 
 # Initialize the variable for the button runApps.
 runApps = tk.Button(root, text="Run Apps", padx=10,
-                    pady=5, fg="white", bg="red", command=runApps)
-runApps.pack()
+                    pady=5, fg="red", bg="white", command=runApps)
+runApps.pack(side=TOP)
 
 
 # Saves the selected apps for the next-time execution.
 for app in apps:
     label = tk.Label(frame, text=app)
     label.pack()
+
+reRun = tk.Button(root, text="Reset", padx=10,
+                  pady=5, fg="purple", bg="white", command=lambda: reset(label))
+reRun.pack(side=RIGHT)
 
 root.mainloop()
 
